f54c39d5cf36b047551caf045f9d90d8de10f411
Shady Hashoul <shady.h@clewmed.com>
2020-11-04 17:33:50 +0200

    Add test script

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -2,20 +2,26 @@ import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
 def call() {
     node {
-      Utils.markStageSkippedForConditional("Audit tools")
-
-      sh '''
-        git version
-        docker version
-        echo "Audit Tools groovy"
-        #dotnet --list-sdks
-        #dotnet --list-runtimes
-        echo "Running from shared library..."
-      '''
+      if (!env.RC)
+      {
+          Utils.markStageSkippedForConditional("Audit tools")
+      }
+      else{
+          sh '''
+                git version
+                docker version
+                echo "Audit Tools groovy"
+                #dotnet --list-sdks
+                #dotnet --list-runtimes
+                echo "Running from shared library..."
+              '''
       env.JENKINS_SKIP_RELEASE = 'TRUE'
       withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'NEXUS_PASSWORD', usernameVariable: 'USERNAME', 
                       passwordVariable: 'NEXUS_PASSWORD']]){
                           sh(libraryResource('test.sh'))
             }
     }
+      }
+
+      
 }
