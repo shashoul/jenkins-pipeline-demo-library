6afaa7fdfc20642f6bd0c20dba471066670aa3da
Shady Hashoul <shady.h@clewmed.com>
2020-11-04 15:54:12 +0200

    Add test script

--- resources/test.sh
+++ resources/test.sh
@@ -4,4 +4,7 @@ echo "Hi There , Running from resources..."
 echo "BUILD NUMBER ${BUILD_NUMBER}"
 echo "WORKSPACE ${WORKSPACE}"
 echo "JOB_NAME ${JOB_NAME}"
+echo "BRANCH_NAME ${BRANCH_NAME}"
+echo "NEXUS_PASSWORD ${NEXUS_PASSWORD}"
+echo "NEXUS_USERNAME ${USERNAME}"
 ls -la ${WORKSPACE}
